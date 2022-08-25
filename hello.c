b32e89119f0d8bb173e4bdf69c91d22d1d5afca2
Jonathan <111910830+Jas069@users.noreply.github.com>
2022-08-25 19:05:14 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Andrade, Jonathan
+ * 2022Jun23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+    int main(int argc, char **argv) {
+
+    printf("My name is Jonathan i think?\n");
+
+  return 0;
+}
